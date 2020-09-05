ab78cc6b396e9b59aa52596babdd77960e4712e3
Ryota Inagaki <66146094+RyotaInagaki1@users.noreply.github.com>
2020-09-04 20:11:55 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct Node{
@@ -38,12 +30,31 @@ void clean(Node* a){
         printf("Cleand");
     }
 }
+void PreOrder(Node* a){
+    if (a!=NULL){
+        int number=a -> len;
+        Node** lis=a->l;
+        printf("%d\n", a->a);
+        for (int i=0; i<number; i++){
+            PreOrder(lis[i]);
+        }
+    }
+}
 int main(){
     Node* thing=newNode();
     Node** things=malloc(2*sizeof(Node*));
     things[0]=thing;
     things[1]=thing;
-    Node* thing2=add(thing, 3, 2);
-    clean(thing2);
+    Node* thing2=add(things, 3, 2);
+    Node** things2=malloc(2*sizeof(Node*));
+    things2[0]=thing;
+    things2[1]=thing;
+    Node* thing3=add(things, 4, 2);
+    Node** things3=malloc(2*sizeof(Node*));
+    things3[0]=thing2;
+    things3[1]=thing3;
+    Node* thing4=add(things3, 5, 2);
+    PreOrder(thing4);
+    clean(thing4);
     return 0;
 }
